f16089fbe012fe8494730bc252b7e43fc4b9bc76
Kachmar Ayman <ayman.kachmar@etu.u-bordeaux.fr>
2021-04-03 18:32:57 +0200

    ajout coups valides et amélioration partie

--- src/Jeu/Plateau.java
+++ src/Jeu/Plateau.java
@@ -1,8 +1,7 @@
 package Jeu;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 /**
  *
@@ -10,14 +9,19 @@ import java.util.Map;
  */
 public class Plateau {
 
+    private static Scanner in = new Scanner(System.in);
+    private static PrintStream out = System.out;
+
     private final int nbColonnes, nbLignes;
     private final Case[][] plat;
+    private boolean casesAlignees;
 
     public Plateau(int nbLignes, int nbColonnes) {
         this.nbColonnes = nbColonnes;
         this.nbLignes = nbLignes;
         this.plat = new Case[nbLignes][nbColonnes];
         initPlateau();
+        this.casesAlignees = false;
     }
 
     /**
@@ -34,42 +38,73 @@ public class Plateau {
     }
 
     /**
+     * 
+     * 
+     */
+    private void etatPlateau() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                Case c = plat[i][j];
+                casesAlignees = c.victoireAlignement(this);
+            }
+        }
+    }
+
+    public boolean partieTerminee() {
+        etatPlateau();
+
+        return casesAlignees;
+    }
+
+    /**
      * Afficher l'ensemble du plateau
      * 
      */
     public void display() {
+
         char col = 65;
-        System.out.print("    ");
+        out.print("    ");
         for (int c = 0; c < nbColonnes; c++) {
 
-            System.out.print(col);
-            System.out.print(" ");
+            out.print(col);
+            out.print(" ");
             col++;
         }
 
-        System.out.println();
+        out.println();
 
         displayBar();
 
         for (int r = 0; r < nbLignes; r++) {
-            System.out.print(r);
+            out.print(r);
 
             if (r >= 10) {
-                System.out.print("|");
+                out.print("|");
             } else {
-                System.out.print(" |");
+                out.print(" |");
             }
 
             for (int c = 0; c < nbColonnes; c++) {
+                out.print(" ");
+                out.print(plat[r][c].getAffichable());
 
-                System.out.print(plat[r][c].getAffichable());
-                System.out.print(" ");
             }
 
-            System.out.println(" |");
+            out.println(" |");
 
         }
         displayBar();
+    }
+
+    public void modifPlat(Partie partie, boolean estNoir, String coup) {
+
+        int cC = coupCol(coup);
+        int cL = coupLigne(coup);
+        Coordonnees c = new Coordonnees(cC, cL);
+
+        if (c.estDansPlateau(this)) {
+            plat[cL][cC].setColor(estNoir);
+        }
     }
 
     /**
@@ -77,11 +112,33 @@ public class Plateau {
      * 
      */
     private void displayBar() {
-        System.out.print("  +");
+        out.print("  +");
         for (int c = 0; c < nbColonnes; c++) {
-            System.out.print(" -");
+            out.print(" -");
         }
-        System.out.println(" +");
+        out.println(" +");
+    }
+
+    public int coupCol(String coup) {
+        char charCol = coup.charAt(0);
+        int colToInt = Coordonnees.carColVersNum(charCol, getNbColonnes());
+
+        return colToInt;
+    }
+
+    public int coupLigne(String coup) {
+        char charLig1 = coup.charAt(1);
+        char charLig2;
+        String nb = "" + charLig1;
+        int lig = 0;
+
+        if (coup.length() == 3) {
+            charLig2 = coup.charAt(2);
+            nb += "" + charLig2;
+        }
+
+        lig = Integer.parseInt(nb);
+        return lig;
     }
 
     /**
